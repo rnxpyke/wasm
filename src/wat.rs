9743021802c39cbc7f9dfc76f705c398db0b9660
rnxpyke <rnxpyke@gmail.com>
2024-01-03 22:34:17 +0100

    fix number handling stuff

--- src/wat.rs
+++ src/wat.rs
@@ -6,7 +6,7 @@ pub enum Token {
     RightParen,
     Atom(String),
     Text(String),
-    Number(isize),
+    Number(String),
     Name(String),
     Comment(String),
 }
@@ -20,6 +20,133 @@ pub enum TokenizeError {
 
 pub struct Tokenizer<'s> {
     input: &'s str
+}
+
+pub struct GostyleTokenizer<'s> {
+    input: &'s str,
+    pos: usize,
+}
+
+impl<'s> GostyleTokenizer<'s> {
+    fn emit(&mut self) -> &'s str {
+        let (tok, rest) = self.input.split_at(self.pos);
+        self.input = rest;
+        self.pos = 0;
+        return tok;
+    }
+
+    fn skip(&mut self) {
+        let (_, rest) = self.input.split_at(self.pos);
+        self.input = rest;
+        self.pos = 0;
+    }
+
+    fn current(&mut self) -> &'s str {
+        let (_, cur) = self.input.split_at(self.pos);
+        return cur;
+    }
+
+    fn expectChar(&mut self, c: char) -> Result<(), TokenizeError> {
+        if self.acceptChar(c) {
+            Ok(())
+        } else {
+            Err(TokenizeError::FailedExpectedToken)
+        }
+    }
+
+    fn acceptChar(&mut self, c: char) -> bool {
+        if self.current().starts_with(c) {
+            self.pos += 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn accept(&mut self, variants: &[char]) -> bool {
+        for variant in variants {
+            if self.acceptChar(*variant) {
+                return true
+            }
+        }
+        return false;
+    }
+
+    fn expect(&mut self, variants: &[char]) -> Result<(), TokenizeError> {
+      if self.accept(variants) {
+        Ok(())
+      } else {
+        Err(TokenizeError::FailedExpectedToken)
+      }
+    }
+
+    fn acceptDigit(&mut self) -> bool {
+        const DIGITS: &'static [char] = &['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        return self.accept(DIGITS)
+    }
+
+    fn acceptLetter(&mut self) -> bool {
+        let Some(c) = self.current().chars().nth(1) else { return false };
+        if c.is_alphabetic() {
+            self.pos += 1;
+            return true;
+        }
+        return false;
+    }
+
+    fn acceptNameChar(&mut self) -> bool {
+        const SPECIALS: &'static [char] = &['_', '.', '+', '-', '*', '/'];
+        if self.accept(SPECIALS) {
+            return true;
+        }
+        if self.acceptLetter() {
+            return true;
+        }
+        if self.acceptDigit() {
+            return true;
+        }
+        return false;
+    }
+
+    fn expectName(&mut self) -> Result<(), TokenizeError> {
+        self.expectChar('$')?;
+        loop {
+            if !self.acceptNameChar() { break }
+        }
+        Ok(())
+    }
+
+    fn expectNumber(&mut self) -> Result<(), TokenizeError> {
+        const DIGITS: &'static [char] = &['_', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+        self.accept(&['+', '-']);
+        if self.acceptChar('0') {
+            if self.acceptChar('x') {
+                // parsing hex digint
+            }
+        }
+        loop {
+            let had_digit = self.accept(DIGITS);
+            if !had_digit { break; }
+        }
+        if self.acceptChar('.') {
+            //number is fractional
+            loop {
+                let had_digit = self.accept(DIGITS);
+                if !had_digit { break; }
+            }
+        }
+        if self.accept(&['e', 'E']) {
+            // accept exponent
+            loop {
+                let had_digit = self.accept(DIGITS);
+                if !had_digit { break; }
+            }
+        }
+        if self.pos == 0 {
+            return Err(TokenizeError::FailedExpectedToken);
+        }
+        Ok(())
+    }
 }
 
 impl Tokenizer<'_> {
@@ -46,9 +173,11 @@ impl Tokenizer<'_> {
     }
 
     fn try_name(&mut self) -> Result<Token, TokenizeError> {
-        self.expect("$")?;
-        let Ok(Token::Atom(s)) = self.try_atom() else { return Err(TokenizeError::FailedExpectedToken) };
-        return Ok(Token::Name(s));
+        let mut gostyle = GostyleTokenizer { input: self.input, pos: 0 };
+        gostyle.expectName()?;
+        let name = gostyle.emit();
+        self.input = gostyle.input;
+        return Ok(Token::Name(name.into()));
     }
 
     fn try_string(&mut self) -> Result<Token, TokenizeError> {
@@ -95,20 +224,11 @@ impl Tokenizer<'_> {
     }
 
     fn try_number(&mut self) -> Result<Token, TokenizeError> {
-        let mut pos = 0;
-        let mut num: isize = 0;
-        let mut it = self.input.char_indices();
-
-        loop {
-            let Some((idx, char)) = it.next() else { break };
-            let Some(digit) = char.to_digit(10) else { break };
-            num *= 10;
-            num += digit as isize;
-            pos = idx;
-        }
-        let (_, rest) = self.input.split_at(pos+1);
-        self.input = rest;
-        return Ok(Token::Number(num))
+        let mut gostyle = GostyleTokenizer { input: self.input, pos: 0 };
+        gostyle.expectNumber()?;
+        let num = gostyle.emit();
+        self.input = gostyle.input;
+        return Ok(Token::Number(num.into()));
     }
 
     fn try_comment(&mut self) -> Result<Token, TokenizeError> {
@@ -148,12 +268,8 @@ impl Tokenizer<'_> {
             '$' => self.try_name().map(Some),
             '"' => self.try_string().map(Some),
             ';' => self.try_comment().map(Some),
-            '+' => { self.expect("+"); self.try_number().map(Some) }
-            '-' => { 
-                self.expect("-");  
-                let Token::Number(x) = self.try_number()? else { return Err(TokenizeError::FailedExpectedToken) };
-                return Ok(Some(Token::Number(-x)))
-            }
+            '+' => self.try_number().map(Some),
+            '-' => self.try_number().map(Some),
             a if a.is_alphabetic() => self.try_atom().map(Some),
             d if d.is_ascii_digit() => self.try_number().map(Some),
             _ => Err(TokenizeError::UnexpectedNextChar(next_char)),
